7e879d2559690a4a4a3ec56ea858d3b80a254541
Jason Gong <mhpjay@gmail.com>
2020-06-29 18:11:18 -0400

    move index.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 
 module.exports = {
     context: __dirname,
-    entry: "./src/index.js",
+    entry: "./index.js",
     output: {
         filename: "main.js", 
         path: path.resolve(__dirname, './'),
